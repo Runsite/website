25593297ab4ad238dee42b3dc3d08d8f8228ceb7
Jaroslaw Goszowski <goszowski@gmail.com>
2018-03-23 17:18:52 +0200

    Initial commit

---
+++ views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi eaque molestiae, molestias sint dolorem tenetur iure, distinctio optio dignissimos pariatur delectus vero laboriosam vitae quia quibusdam eveniet aspernatur voluptas officia.
+	</p>
+@endsection
---
+++ views/layouts/app.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.resources')
+
+@section('app')
+	@yield('content')
+@endsection
