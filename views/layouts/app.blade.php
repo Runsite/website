4d91dea4e92cef572c05b40ef94ab2157785c865
Jaroslaw Goszowski <goszowski@gmail.com>
2018-03-30 21:06:16 +0300

    Додав rel prefetch

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -16,19 +16,19 @@
 							<div class="collapse navbar-collapse" id="mainNav" aria-label="Навігація по сайту">
 								<ul class="navbar-nav ml-auto">
 									<li class="nav-item">
-										<a class="nav-link" href="#">Що ми робимо</a>
+										<a class="nav-link" href="#" rel="prefetch">Що ми робимо</a>
 									</li>
 									<li class="nav-item">
-										<a class="nav-link" href="#">Портфоліо</a>
+										<a class="nav-link" href="#" rel="prefetch">Портфоліо</a>
 									</li>
 									<li class="nav-item">
-										<a class="nav-link" href="#">Особливості</a>
+										<a class="nav-link" href="#" rel="prefetch">Особливості</a>
 									</li>
 									<li class="nav-item">
-										<a class="nav-link" href="#">Наші клієнти</a>
+										<a class="nav-link" href="#" rel="prefetch">Наші клієнти</a>
 									</li>
 									<li class="nav-item">
-										<a class="nav-link" href="#">Контакти</a>
+										<a class="nav-link" href="#" rel="prefetch">Контакти</a>
 									</li>
 								</ul>
 							</div>
@@ -38,7 +38,7 @@
 					<div class="blind bg-white to-right d-lg-none"></div>
 				</div>
 				<div class="col-lg-2 pl-0 hidden d-none d-lg-block">
-					<a href="#" class="btn btn-dark btn-lg btn-block btn-header js-btn-cursor-spot js-btn-cursor-spot-light"><i class="fa fa-edit"></i> Бриф</a>
+					<a href="#" class="btn btn-dark btn-lg btn-block btn-header js-btn-cursor-spot js-btn-cursor-spot-light" rel="prefetch"><i class="fa fa-edit"></i> Бриф</a>
 				</div>
 			</div>
 		</div>
