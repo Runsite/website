a810905a96b61ac2d2867ed3f302f1f01a442a69
Jaroslaw Goszowski <goszowski@gmail.com>
2018-03-30 20:19:15 +0300

    Футер готовий

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -49,26 +49,93 @@
 	<footer class="bg-dark text-white">
 		<div class="container pt-5 pb-6">
 			<div class="row">
-				<div class="col-lg-4 pr-6">
-					<h2 class="footer-h"><span>Що ми робимо</span></h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla sint culpa vel similique eveniet repudiandae id consequuntur at velit quisquam! Fugiat autem architecto praesentium amet ad a fuga recusandae sed.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla sint culpa vel similique eveniet repudiandae id consequuntur at velit quisquam! Fugiat autem architecto praesentium amet ad a fuga recusandae sed.</p>
+				<div class="col-md-4">
+					<section class="pr-lg-6">
+						<h2 class="footer-h"><span>Що ми робимо</span></h2>
+						<nav class="nav-footer mt-4">
+							<menu type="toolbar">
+								<li><a href="#">Сайт-візитка</a></li>
+								<li><a href="#">Корпоративний сайт</a></li>
+								<li><a href="#">Сайт для ЗМІ</a></li>
+								<li><a href="#">Інтернет-магазин</a></li>
+								<li><a href="#">Технічний аудит</a></li>
+								<li><a href="#">SEO аудит</a></li>
+								<li><a href="#">Технічна підтримка</a></li>
+								<li><a href="#">Наповнення контентом</a></li>
+								<li><a href="#">Хостинг</a></li>
+								<li><a href="#">SEO оптимізація</a></li>
+								<li><a href="#">Розробка ПЗ</a></li>
+							</menu>
+						</nav>
+					</section>
 				</div>
-				<div class="col-lg-4 pl-5 pr-5">
-					<h2 class="footer-h"><span>Наш блог</span></h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla sint culpa vel similique eveniet repudiandae id consequuntur at velit quisquam! Fugiat autem architecto praesentium amet ad a fuga recusandae sed.</p>
-					<h2 class="footer-h mt-5"><span>Потенційним клієнтам</span></h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse iste eveniet natus id impedit velit error mollitia, dolores deleniti, vitae asperiores nisi minus veniam fugiat, tempora animi debitis laborum maiores.</p>
+				<div class="col-md-4 pt-5 pt-md-0">
+					<div class="pl-lg-5 pr-lg-5">
+						<section>
+							<h2 class="footer-h"><span>Наш блог</span></h2>
+							<nav class="nav-footer mt-4">
+								<menu type="toolbar">
+									<li><a href="#">Переваги Angular</a></li>
+									<li><a href="#">Недоліки Wordpress</a></li>
+									<li><a href="#">Нові технології адаптації</a></li>
+									<li><a href="#">Інтернет-магазин з нуля</a></li>
+								</menu>
+							</nav>
+						</section>
+						<section>
+							<h2 class="footer-h mt-5"><span>Потенційним клієнтам</span></h2>
+							<nav class="nav-footer mt-3">
+								<menu type="toolbar">
+									<li><a href="#">Розрахувати вартість сайту</a></li>
+									<li><a href="#">Задати питання</a></li>
+									<li><a href="#">Заповнити бриф</a></li>
+									<li><a href="#">Зв’язатись з нами</a></li>
+								</menu>
+							</nav>
+						</section>
+					</div>
 				</div>
-				<div class="col-lg-4 pl-6">
-					<h2 class="footer-h"><span>Контакти</span></h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla sint culpa vel similique eveniet repudiandae id consequuntur at velit quisquam! Fugiat autem architecto praesentium amet ad a fuga recusandae sed.</p>
+				<div class="col-md-4 pt-5 pt-md-0">
+					<div class="pl-lg-6">
+						<h2 class="footer-h"><span>Контакти</span></h2>
+						<nav class="nav-footer mt-4">
+							<menu type="toolbar">
+								<li class="has-icons mt-4">
+									<span class="nav-footer-icon">
+										<i class="fas fa-map-marker-alt"></i>
+									</span>
+									<span>
+										вул. Саксаганського 53/80 <br>
+										м. Київ, Україна
+									</span>
+								</li>
+								<li class="has-icons mt-3">
+									<span class="nav-footer-icon">
+										<i class="fas fa-phone"></i>
+									</span>
+									<span>
+										<a href="tel:+380634242623" data-toggle="tooltip" title="Зателефонувати зараз" class="p-0 d-inline">38 (063) 42 42 623</a><br>
+										<a href="tel:+380730089771" data-toggle="tooltip" title="Зателефонувати зараз" class="p-0 d-inline">38 (073) 00 89 771</a>
+									</span>
+								</li>
+								<li class="has-icons mt-3">
+									<span class="nav-footer-icon">
+										<i class="fas fa-envelope"></i>
+									</span>
+									<span>
+										<a href="mailto:info@runsite.com.ua" data-toggle="tooltip" title="Написати зараз" class="p-0 d-inline">info@runsite.com.ua</a><br>
+										<a href="mailto:pr@runsite.com.ua" data-toggle="tooltip" title="Написати зараз" class="p-0 d-inline">pr@runsite.com.ua</a>
+									</span>
+								</li>
+							</menu>
+						</nav>
 
-					<p class="pt-3 pb-3">
-						<a href="#" rel="nofollow" target="_blank" class="btn btn-lg btn-block btn-primary js-btn-cursor-spot js-btn-cursor-spot-light"><small><i class="fab fa-facebook"></i> Facebook</small></a>
-					</p>
+						<p class="pt-4 pb-4 mb-0">
+							<a href="#" data-toggle="tooltip" title="Наша сторінка на Facebook" rel="nofollow" target="_blank" class="btn btn-lg btn-block btn-primary js-btn-cursor-spot js-btn-cursor-spot-light"><small><i class="fab fa-facebook"></i> Facebook</small></a>
+						</p>
 
-					<p class="text-muted">© Copyright 2016-{{ date('Y') }} Рансайт ТОВ</p>
+						<p class="text-muted">© Copyright 2016-{{ date('Y') }} Рансайт ТОВ</p>
+					</div>
 				</div>
 			</div>
 		</div>
--- views/layouts/resources.blade.php
+++ views/layouts/resources.blade.php
@@ -15,11 +15,14 @@
 	@yield('app')
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	<script src="/assets/bootstrap-4.0.0/dist/js/bootstrap.min.js"></script>
 	<script src="/assets/node_modules/pace-js/pace.min.js"></script>
 
 	<script>
 		$(function() {
+			$('[data-toggle="tooltip"]').tooltip();
+
 			$('.js-btn-cursor-spot').each(function() {
 				$(this).append('<span class="js-spot"></span>');
 			});
