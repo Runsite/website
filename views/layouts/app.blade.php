312ddf8f87a1efa458bdbbf0dddcfb0bc493bcf1
Jaroslaw Goszowski <goszowski@gmail.com>
2018-03-30 18:52:05 +0300

    Розмітка футера

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -45,4 +45,32 @@
 	</div>
 
 	@yield('content')
+
+	<footer class="bg-dark text-white">
+		<div class="container pt-5 pb-6">
+			<div class="row">
+				<div class="col-lg-4 pr-6">
+					<h2 class="footer-h"><span>Що ми робимо</span></h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla sint culpa vel similique eveniet repudiandae id consequuntur at velit quisquam! Fugiat autem architecto praesentium amet ad a fuga recusandae sed.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla sint culpa vel similique eveniet repudiandae id consequuntur at velit quisquam! Fugiat autem architecto praesentium amet ad a fuga recusandae sed.</p>
+				</div>
+				<div class="col-lg-4 pl-5 pr-5">
+					<h2 class="footer-h"><span>Наш блог</span></h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla sint culpa vel similique eveniet repudiandae id consequuntur at velit quisquam! Fugiat autem architecto praesentium amet ad a fuga recusandae sed.</p>
+					<h2 class="footer-h mt-5"><span>Потенційним клієнтам</span></h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse iste eveniet natus id impedit velit error mollitia, dolores deleniti, vitae asperiores nisi minus veniam fugiat, tempora animi debitis laborum maiores.</p>
+				</div>
+				<div class="col-lg-4 pl-6">
+					<h2 class="footer-h"><span>Контакти</span></h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla sint culpa vel similique eveniet repudiandae id consequuntur at velit quisquam! Fugiat autem architecto praesentium amet ad a fuga recusandae sed.</p>
+
+					<p class="pt-3 pb-3">
+						<a href="#" rel="nofollow" target="_blank" class="btn btn-lg btn-block btn-primary js-btn-cursor-spot js-btn-cursor-spot-light"><small><i class="fab fa-facebook"></i> Facebook</small></a>
+					</p>
+
+					<p class="text-muted">© Copyright 2016-{{ date('Y') }} Рансайт ТОВ</p>
+				</div>
+			</div>
+		</div>
+	</footer>
 @endsection
